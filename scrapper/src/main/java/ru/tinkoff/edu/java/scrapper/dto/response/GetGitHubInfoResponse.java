f1ba774b014542222bea67431a10d7a1b761cf35
miqqra <mikhail.krasnikov.04@gmail.com>
2023-03-30 17:36:26 +0700

    Fix client

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/dto/response/GetGitHubInfoResponse.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/dto/response/GetGitHubInfoResponse.java
@@ -1,4 +1,8 @@
 package ru.tinkoff.edu.java.scrapper.dto.response;
 
-public record GetGitHubInfoResponse(GitHubItems[] items) {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.OffsetDateTime;
+
+public record GetGitHubInfoResponse(@JsonProperty("updated_at") OffsetDateTime lastUpdate) {
 }
--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/dto/response/GetStackOverflowInfoResponse.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/dto/response/GetStackOverflowInfoResponse.java
@@ -1,3 +1,17 @@
 package ru.tinkoff.edu.java.scrapper.dto.response;
 
-public record GetStackOverflowInfoResponse (StackOverflowItemsResponse[] items){}
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Map;
+
+public class GetStackOverflowInfoResponse {
+    OffsetDateTime lastActivityDate;
+
+    @JsonProperty("items")
+    private void unpackNameFromNestedObject(List<Map<String, String>> items) {
+        lastActivityDate = OffsetDateTime.parse(items.get(0).get("last_activity_date"));
+    }
+}
+
