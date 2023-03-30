86777019aad37ffd4403e2594b78362139dea547
miqqra <mikhail.krasnikov.04@gmail.com>
2023-03-30 17:49:30 +0700

    Delete requestMapping annotation

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/controller/ScrapperController.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/controller/ScrapperController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.tinkoff.edu.java.scrapper.dto.request.AddLinkRequest;
@@ -23,7 +22,6 @@ import ru.tinkoff.edu.java.scrapper.dto.response.ListLinksResponse;
 import ru.tinkoff.edu.java.scrapper.service.ScrapperService;
 
 @RestController
-@RequestMapping
 @RequiredArgsConstructor
 public class ScrapperController {
     private final ScrapperService scrapperService;
