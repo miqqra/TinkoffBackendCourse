22144dc93ffbac7be57b40f4c2a5f50d9b7a8893
miqqra <mikhail.krasnikov.04@gmail.com>
2023-03-30 17:49:30 +0700

    Delete requestMapping annotation

--- bot/src/main/java/ru/tinkoff/edu/java/bot/controller/BotController.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/controller/BotController.java
@@ -6,13 +6,11 @@ import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.tinkoff.edu.java.bot.dto.request.LinkUpdate;
 import ru.tinkoff.edu.java.bot.service.BotService;
 
 @RestController
-@RequestMapping()
 @RequiredArgsConstructor
 public class BotController {
     private final BotService botService;
