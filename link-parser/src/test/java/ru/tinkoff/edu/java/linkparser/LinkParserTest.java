cd45f4144b897b1b8d15de6324cdfff8cc1db77d
miqqra <mikhail.krasnikov.04@gmail.com>
2023-04-11 02:58:37 +0700

    Change linkparser tests to parameterized

--- link-parser/src/test/java/ru/tinkoff/edu/java/linkparser/LinkParserTest.java
+++ link-parser/src/test/java/ru/tinkoff/edu/java/linkparser/LinkParserTest.java
@@ -2,7 +2,8 @@ package ru.tinkoff.edu.java.linkparser;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.tinkoff.edu.java.linkparser.parseResult.GitHubParseResult;
 import ru.tinkoff.edu.java.linkparser.parseResult.ParseResult;
@@ -13,105 +14,75 @@ import java.net.URL;
 
 @SpringBootTest(classes = {LinkParser.class})
 public class LinkParserTest {
+    private final LinkParser linkParser = new LinkParser();
 
-    @Test
-    void parseCorrectStackoverflowURL() throws MalformedURLException {
-        LinkParser linkParser = new LinkParser();
-        ParseResult parseResult1 = linkParser.parse(
-                new URL("https://stackoverflow.com/questions/49733733/open-a-project-in-intellij-from-folder"));
-        ParseResult parseResult2 = linkParser.parse(
-                new URL("https://stackoverflow.com/questions/49733733"));
-        ParseResult parseResult3 = linkParser.parse(
-                new URL("https://stackoverflow.com/questions/49733733/"));
-        ParseResult parseResult4 = linkParser.parse(
-                new URL("https://ru.stackoverflow.com/questions/1510760"));
-        ParseResult parseResult5 = linkParser.parse(
-                new URL("https://ru.stackoverflow.com/questions/1510760/"));
-
-        assertThat(parseResult1, is(notNullValue()));
-        assertThat(parseResult1, is(instanceOf(StackOverflowParseResult.class)));
-        assertThat(((StackOverflowParseResult) parseResult1).getQuestionId(), equalTo(49733733L));
-
-        assertThat(parseResult2, is(notNullValue()));
-        assertThat(parseResult2, is(instanceOf(StackOverflowParseResult.class)));
-        assertThat(((StackOverflowParseResult) parseResult2).getQuestionId(), equalTo(49733733L));
-
-        assertThat(parseResult3, is(notNullValue()));
-        assertThat(parseResult3, is(instanceOf(StackOverflowParseResult.class)));
-        assertThat(((StackOverflowParseResult) parseResult3).getQuestionId(), equalTo(49733733L));
-
-        assertThat(parseResult4, is(notNullValue()));
-        assertThat(parseResult4, is(instanceOf(StackOverflowParseResult.class)));
-        assertThat(((StackOverflowParseResult) parseResult4).getQuestionId(), equalTo(1510760L));
-
-        assertThat(parseResult5, is(notNullValue()));
-        assertThat(parseResult5, is(instanceOf(StackOverflowParseResult.class)));
-        assertThat(((StackOverflowParseResult) parseResult5).getQuestionId(), equalTo(1510760L));
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "https://stackoverflow.com/questions/49733733/open-a-project-in-intellij-from-folder",
+            "https://stackoverflow.com/questions/49733733",
+            "https://stackoverflow.com/questions/49733733/"
+    })
+    void returnsSameStackOverflowQuestionIds(String input) throws MalformedURLException {
+        ParseResult parseResult = linkParser.parse(new URL(input));
+        assertThat(parseResult, is(notNullValue()));
+        assertThat(parseResult, is(instanceOf(StackOverflowParseResult.class)));
+        assertThat(((StackOverflowParseResult) parseResult).getQuestionId(), equalTo(49733733L));
     }
 
-    @Test
-    void parseCorrectGitHubURL() throws MalformedURLException {
-        LinkParser linkParser = new LinkParser();
-        ParseResult parseResult1 = linkParser.parse(
-                new URL("https://github.com/miqqra/TinkoffBackendCourse/pull/3"));
-        ParseResult parseResult2 = linkParser.parse(
-                new URL("https://github.com/miqqra/TinkoffBackendCourse"));
-        ParseResult parseResult3 = linkParser.parse(
-                new URL("https://github.com/miqqra/OOP"));
-        ParseResult parseResult4 = linkParser.parse(
-                new URL("https://github.com/miqqra/TinkoffBackendCourse/"));
-
-        assertThat(parseResult1, is(notNullValue()));
-        assertThat(parseResult1, is(instanceOf(GitHubParseResult.class)));
-        assertThat(((GitHubParseResult) parseResult1).getUser(), equalTo("miqqra"));
-        assertThat(((GitHubParseResult) parseResult1).getRepository(), equalTo("TinkoffBackendCourse"));
-
-        assertThat(parseResult2, is(notNullValue()));
-        assertThat(parseResult2, is(instanceOf(GitHubParseResult.class)));
-        assertThat(((GitHubParseResult) parseResult2).getUser(), equalTo("miqqra"));
-        assertThat(((GitHubParseResult) parseResult2).getRepository(), equalTo("TinkoffBackendCourse"));
-
-        assertThat(parseResult3, is(notNullValue()));
-        assertThat(parseResult3, is(instanceOf(GitHubParseResult.class)));
-        assertThat(((GitHubParseResult) parseResult3).getUser(), equalTo("miqqra"));
-        assertThat(((GitHubParseResult) parseResult3).getRepository(), equalTo("OOP"));
-
-        assertThat(parseResult4, is(notNullValue()));
-        assertThat(parseResult4, is(instanceOf(GitHubParseResult.class)));
-        assertThat(((GitHubParseResult) parseResult4).getUser(), equalTo("miqqra"));
-        assertThat(((GitHubParseResult) parseResult4).getRepository(), equalTo("TinkoffBackendCourse"));
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "https://ru.stackoverflow.com/questions/1510760",
+            "https://ru.stackoverflow.com/questions/1510760/"
+    })
+    void returnsOtherSameStackOverflowQuestionIds(String input) throws MalformedURLException {
+        ParseResult parseResult = linkParser.parse(new URL(input));
+        assertThat(parseResult, is(notNullValue()));
+        assertThat(parseResult, is(instanceOf(StackOverflowParseResult.class)));
+        assertThat(((StackOverflowParseResult) parseResult).getQuestionId(), equalTo(1510760L));
     }
 
-    @Test
-    void parseIncorrectURLs() throws MalformedURLException {
-        LinkParser linkParser = new LinkParser();
-        ParseResult parseResult1 = linkParser.parse(
-                new URL("https://stackoverflow.com/questions/"));
-        ParseResult parseResult2 = linkParser.parse(
-                new URL("https://stackoverflow.com/questions"));
-        ParseResult parseResult3 = linkParser.parse(
-                new URL("https://stackoverflow.com/"));
-        ParseResult parseResult4 = linkParser.parse(
-                new URL("https://ru.stackoverflow.com"));
-        ParseResult parseResult5 = linkParser.parse(
-                new URL("https://ru.stack"));
-        ParseResult parseResult6 = linkParser.parse(
-                new URL("https://github.com/miqqra/"));
-        ParseResult parseResult7 = linkParser.parse(
-                new URL("https://github.com/"));
-        ParseResult parseResult8 = linkParser.parse(
-                new URL("https://github"));
-        ParseResult parseResult9 = linkParser.parse(
-                new URL("https://github.com/miqqra"));
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "https://github.com/miqqra/TinkoffBackendCourse/pull/3",
+            "https://github.com/miqqra/TinkoffBackendCourse",
+            "https://github.com/miqqra/TinkoffBackendCourse/"
+    })
+    void returnsSameGithubData(String input) throws MalformedURLException {
+        ParseResult parseResult = linkParser.parse(new URL(input));
+        assertThat(parseResult, is(notNullValue()));
+        assertThat(parseResult, is(instanceOf(GitHubParseResult.class)));
+        assertThat(((GitHubParseResult) parseResult).getUser(), equalTo("miqqra"));
+        assertThat(((GitHubParseResult) parseResult).getRepository(), equalTo("TinkoffBackendCourse"));
 
-        assertThat(parseResult1, is(nullValue()));
-        assertThat(parseResult2, is(nullValue()));
-        assertThat(parseResult3, is(nullValue()));
-        assertThat(parseResult4, is(nullValue()));
-        assertThat(parseResult5, is(nullValue()));
-        assertThat(parseResult6, is(nullValue()));
-        assertThat(parseResult7, is(nullValue()));
-        assertThat(parseResult8, is(nullValue()));
-        assertThat(parseResult9, is(nullValue()));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "https://github.com/miqqra/OOP"
+    })
+    void returnsOtherGithubData(String input) throws MalformedURLException {
+        ParseResult parseResult = linkParser.parse(new URL(input));
+        assertThat(parseResult, is(notNullValue()));
+        assertThat(parseResult, is(instanceOf(GitHubParseResult.class)));
+        assertThat(((GitHubParseResult) parseResult).getUser(), equalTo("miqqra"));
+        assertThat(((GitHubParseResult) parseResult).getRepository(), equalTo("OOP"));
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "https://stackoverflow.com/questions/",
+            "https://stackoverflow.com/questions",
+            "https://stackoverflow.com/",
+            "https://ru.stackoverflow.com",
+            "https://ru.stack",
+            "https://github.com/miqqra/",
+            "https://github.com/",
+            "https://github",
+            "https://github.com/miqqra"
+    })
+    void parseIncorrectURLs(String input) throws MalformedURLException {
+        assertThat(linkParser.parse(new URL(input)), is(nullValue()));
+
     }
 }
