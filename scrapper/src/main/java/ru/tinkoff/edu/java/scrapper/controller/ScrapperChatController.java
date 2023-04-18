4d23b5290753615b08e51642b3d3c28b2434db29
miqqra <mikhail.krasnikov.04@gmail.com>
2023-04-19 01:32:04 +0700

    Change return value

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/controller/ScrapperChatController.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/controller/ScrapperChatController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.tinkoff.edu.java.scrapper.service.impl.JdbcTgChatService;
+import ru.tinkoff.edu.java.scrapper.service.TgChatService;
 
 @RestController
 @RequiredArgsConstructor
 public class ScrapperChatController {
-    private final JdbcTgChatService tgChatService;
+    private final TgChatService tgChatService;
 
     @PostMapping(value = "/tg-chat/{id}")
     public void registerChat(@PathVariable Long id) {
--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/mapper/ListLinkResponseLinkMapper.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/mapper/ListLinkResponseLinkMapper.java
@@ -7,10 +7,11 @@ import ru.tinkoff.edu.java.scrapper.dto.response.ListLinksResponse;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class ListLinkResponseLinkMapper {
-    public static ListLinksResponse linksToListLinksResponse(List<Link> links) {
+    public static ListLinksResponse linksToListLinksResponse(Collection<Link> links) {
         List<LinkResponse> linkArrayList = new ArrayList<>();
         try {
             for (Link link : links) {
