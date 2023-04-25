97ba82bda329d83d8bd57eed1b0c172a70b63154
miqqra <mikhail.krasnikov.04@gmail.com>
2023-04-25 23:35:12 +0700

    Fix SQL scripts for chat and link tables

--- scrapper/src/main/resources/migrations/init_db/create_chat_table.sql
+++ scrapper/src/main/resources/migrations/init_db/create_chat_table.sql
@@ -1,10 +1,10 @@
-create sequence chat_sequence start with 1 increment by 1;
+create sequence if not exists chat_sequence start with 1 increment by 1;
 
 CREATE TABLE chat
 (
-    id              BIGINT      NOT NULL,
+    id              BIGINT      UNIQUE      DEFAULT nextval('chat_sequence'),
     tgChatId        BIGINT      NOT NULL,
-    trackedLink     BIGINT      NOT NULL,
+    trackedLink     BIGINT,
     FOREIGN KEY (trackedLink) REFERENCES link(id),
 
     CONSTRAINT chat_pk PRIMARY KEY (id)
--- scrapper/src/main/resources/migrations/init_db/create_links_table.sql
+++ scrapper/src/main/resources/migrations/init_db/create_links_table.sql
@@ -3,12 +3,12 @@ drop table if exists link;
 drop sequence if exists chat_sequence;
 drop sequence if exists links_sequence;
 
-create sequence links_sequence start with 1 increment by 1;
+create sequence if not exists links_sequence start with 1 increment by 1;
 
 CREATE TABLE link
 (
-    id      BIGINT          NOT NULL,
-    url     VARCHAR(255)    NOT NULL,
+    id              BIGINT          NOT NULL DEFAULT nextval('links_sequence'),
+    url             VARCHAR(255)    NOT NULL,
 
     CONSTRAINT link_pk PRIMARY KEY (id)
 );
